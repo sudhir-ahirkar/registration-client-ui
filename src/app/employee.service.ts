901a0d28c4981cb523db1aa71b16ad122d681339
sahirkar <sudhir.ahirkar28@gmail.com>
2022-12-07 16:32:53 +0530

    initial commit

---
+++ src/app/employee.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  private baseUrl = 'http://localhost:8080/emp-registration-api/api/v1/employees';
+
+  constructor(private http: HttpClient) { }
+
+  getEmployee(id: any): Observable<any> {
+      let num = id;
+      let queryParams = new HttpParams();
+      queryParams = queryParams.append("id",id);
+      return this.http.get(this.baseUrl+"/es",{params:queryParams});
+//     return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  createEmployee(employee: Object): Observable<Object> {
+    return this.http.post(`${this.baseUrl}`, employee);
+  }
+
+  updateEmployee(id: number, value: any): Observable<Object> {
+    return this.http.put(`${this.baseUrl}/${id}`, value);
+  }
+
+  deleteEmployee(id: number): Observable<any> {
+    return this.http.delete(`${this.baseUrl}/${id}`, { responseType: 'text' });
+  }
+
+  getEmployeesList(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
